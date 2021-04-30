cbc5ebc0fa98e0c811884bebd820323e3f36cb6b
SaiEashwarKS <saieashwar.ks@gmail.com>
2021-05-01 00:13:02 +0530

    made copy ctor; made traversal fns const;  made todo file

--- client.cpp
+++ client.cpp
@@ -32,10 +32,7 @@ int main()
     cout << "\n";
     #endif
 
-    //RBT<int> tree {10, 30, 50, 20};
-
-
-    #if 1
+    #if 0
     vector<int>v{10, 30, 20, 50, 40};
     RBT<int> tree(v.begin(), v.end());
     vector<int> v2 = tree.get_inorder();
@@ -45,5 +42,35 @@ int main()
     }
     cout << "\n";
     #endif
+    
+    #if 1
+    vector<int>v{10, 30, 20, 50, 40};
+    RBT<int> tree(v.begin(), v.end());
+    RBT<int> tree2(tree);
+    vector<int> v2 = tree2.get_preorder();
+    for(auto e: v2)
+    {
+        cout << e << "\t";
+    }
+    cout << "\n";
+    #endif
+
+    #if 0
+    RBT<vector<int>> tree;
+    tree.insert({100, 2, 3});
+    tree.insert({10, 20, 30});
+    tree.insert({10, 20, 30});
+    vector<vector<int>> v = tree.get_preorder();
+    for(auto e: v)
+    {
+        for(auto e2 : e)
+            cout << e2 << "\t";
+        cout << "\n";
+    }
+    cout << "\n";
+    #endif
+
+    
+
 	return 0;
 }
